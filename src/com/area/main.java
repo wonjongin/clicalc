733a1a665a8efcbc9f6e77954d05aa3ffdf71969
tnsgud <60930113+tnsgud@users.noreply.github.com>
2020-02-16 01:16:38 +0900

    2020

--- src/com/area/main.java
+++ src/com/area/main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args){
-        System.out.println("넓이 계산 페이지 입니다.\n1:원, 3:삼각형");
+        System.out.println("넓이 계산 페이지 입니다!\n1:원, 3:삼각형");
         Scanner sc = new Scanner(System.in);
 
         circle circle = new circle();
